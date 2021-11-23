42df2c4aa7e697e667b9feb78a7e4b2a553fdbf1
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2021-11-24 01:20:39 +0530

    add binary search

---
+++ divide-and-conquer/binary-search.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+bool binarySearch(int array[], int start, int end, int search);
+
+int main()
+{
+    cout << "Enter 10 elements (in ascending order) " << endl;
+
+    int array[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> array[i];
+    }
+
+    int search;
+    cout << "Enter element to be searched: ";
+    cin >> search;
+
+    bool result = binarySearch(array, 0, 10, search);
+
+    if (result == true)
+    {
+        cout << "Element found" << endl;
+    }
+
+    else
+    {
+        cout << "Element not found" << endl;
+    }
+}
+
+bool binarySearch(int array[], int start, int end, int search)
+{
+    if (start > end)
+        return false;
+
+    int middle = (start + end) / 2;
+
+    if (search == array[middle])
+        return true;
+
+    else
+    {
+        if (search > array[middle])
+            binarySearch(array, middle + 1, end, search);
+
+        else
+            binarySearch(array, start, middle - 1, search);
+    }
+}
