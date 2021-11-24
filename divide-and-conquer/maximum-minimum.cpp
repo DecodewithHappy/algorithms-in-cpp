3b5afece86404ec00283174378df5c9a3280f980
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2021-11-25 01:39:57 +0530

    add maximum minimum using divide and conquer

--- divide-and-conquer/maximum-minimum.cpp
+++ divide-and-conquer/maximum-minimum.cpp
@@ -2,6 +2,76 @@
 
 using namespace std;
 
+class MaxMin
+{
+public:
+    int maximum;
+    int minimum;
+};
+
+MaxMin maximumMinimum(int array[], int start, int end);
+
 int main()
 {
+    int n;
+    MaxMin maxMinPair;
+    maxMinPair.minimum = 0;
+    maxMinPair.maximum = 9;
+
+    cout << "Enter the number of elements in array: ";
+    cin >> n;
+
+    int array[n];
+
+    cout << "Enter " << n << " elements" << endl;
+    for (int i = 0; i < n; i++)
+        cin >> array[i];
+
+    maxMinPair = maximumMinimum(array, 0, n - 1);
+    cout << "Maximum: " << maxMinPair.maximum << endl;
+    cout << "Minimum: " << maxMinPair.minimum << endl;
+}
+
+MaxMin maximumMinimum(int array[], int start, int end)
+{
+
+    MaxMin maxMin;
+
+    if (start == end)
+    {
+        maxMin.maximum = array[start];
+        maxMin.minimum = array[start];
+    }
+    else if (end - start == 1)
+    {
+        if (array[start] > array[end])
+        {
+            maxMin.maximum = array[start];
+            maxMin.minimum = array[end];
+        }
+        else
+        {
+
+            maxMin.maximum = array[end];
+            maxMin.minimum = array[start];
+        }
+    }
+    else
+    {
+        int middle = (start + end) / 2;
+        MaxMin maxMin1 = maximumMinimum(array, start, middle);
+        MaxMin maxMin2 = maximumMinimum(array, middle + 1, end);
+
+        if (maxMin1.maximum > maxMin2.maximum)
+            maxMin.maximum = maxMin1.maximum;
+        else
+            maxMin.maximum = maxMin2.maximum;
+
+        if (maxMin1.minimum < maxMin2.minimum)
+            maxMin.minimum = maxMin1.minimum;
+        else
+            maxMin.minimum = maxMin2.minimum;
+    }
+
+    return maxMin;
 }
