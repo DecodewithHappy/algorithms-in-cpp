06a152b94c4a4482a965867b2e35f8bcf53bf370
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2021-11-25 02:27:17 +0530

    init quick sort

---
+++ divide-and-conquer/quick-sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void displayArray(int array[], int start, int end);
+void quickSort(int array[], int start, int end);
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of elements in array: ";
+    cin >> n;
+
+    int array[n];
+
+    cout << "Enter " << n << " elements" << endl;
+    for (int i = 0; i < n; i++)
+        cin >> array[i];
+
+    quickSort(array, 0, n - 1);
+
+    cout << "After Sorting" << endl;
+    displayArray(array, 0, n - 1);
+}
+
+void displayArray(int array[], int start, int end)
+{
+    for (int i = start; i <= end; i++)
+        cout << array[i] << "\t";
+}
