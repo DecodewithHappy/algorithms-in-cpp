52686379c82616ffc23f5c6f2e4b3dd52b91f168
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2021-11-25 01:56:28 +0530

    add displayArray function

--- divide-and-conquer/merge-sort.cpp
+++ divide-and-conquer/merge-sort.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+void displayArray(int array[], int start, int end);
+
 int main()
 {
+    int n;
+
+    cout << "Enter the number of elements in array: ";
+    cin >> n;
+
+    int array[n];
+
+    cout << "Enter " << n << " elements" << endl;
+    for (int i = 0; i < n; i++)
+        cin >> array[i];
+
+    cout << "Before Sorting" << endl;
+    displayArray(array, 0, n - 1);
+}
+
+void displayArray(int array[], int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        cout << array[i] << "\t";
+    }
 }
