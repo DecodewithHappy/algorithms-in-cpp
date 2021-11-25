27e299e59cbd739a07db9cbe45d2a63524ff9009
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2021-11-25 23:27:32 +0530

    add partition and swap functions

--- divide-and-conquer/quick-sort.cpp
+++ divide-and-conquer/quick-sort.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void displayArray(int array[], int start, int end);
 void quickSort(int array[], int start, int end);
 int partition(int array[], int start, int end);
+void swap(int array[], int left, int right);
 
 int main()
 {
@@ -19,7 +20,7 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> array[i];
 
-    quickSort(array, 0, n - 1);
+    quickSort(array, 0, n);
 
     cout << "After Sorting" << endl;
     displayArray(array, 0, n - 1);
@@ -29,6 +30,8 @@ void displayArray(int array[], int start, int end)
 {
     for (int i = start; i <= end; i++)
         cout << array[i] << "\t";
+
+    puts("");
 }
 
 void quickSort(int array[], int start, int end)
@@ -39,4 +42,37 @@ void quickSort(int array[], int start, int end)
     int boundary = partition(array, start, end);
     quickSort(array, start, boundary);
     quickSort(array, boundary + 1, end);
+}
+
+int partition(int array[], int start, int end)
+{
+    int pivot = array[start];
+    int left = start;
+    int right = end;
+
+    while (left < right)
+    {
+        do
+        {
+            left++;
+        } while (array[left] <= pivot);
+
+        do
+        {
+            right--;
+        } while (array[right] > pivot);
+
+        if (left < right)
+            swap(array, left, right);
+    }
+    swap(array, start, right);
+
+    return right;
+}
+
+void swap(int array[], int left, int right)
+{
+    int temp = array[left];
+    array[left] = array[right];
+    array[right] = temp;
 }
