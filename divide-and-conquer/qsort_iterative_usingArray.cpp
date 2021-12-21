c5fe65cbb3bf63f4f0479614c81da296ed0f9e18
Happy <poddarsayan22@gmail.com>
2021-12-21 20:29:51 +0530

    port c to c++

--- divide-and-conquer/qsort_iterative_usingArray.cpp
+++ divide-and-conquer/qsort_iterative_usingArray.cpp
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <iostream>
+using namespace std;
+
 #define MAX 50
 
 int main(){
@@ -8,19 +9,19 @@ int main(){
    int partition(int [], int *, int, int);
    int quicksort(int [], int);
    int a[50], n, i, x;
-   printf("Enter the total number of elements: \n");
-   scanf("%d",&n);
-   printf("Enter array elements: \n");
+   cout << "Enter the total number of elements:" << "\n";
+   cin >> n;
+   cout << "Enter array elements:" << "\n";
    for(i=0; i<n; i++)
    {
-      scanf("%d",&a[i]);
+      cin >> a[i];
    }
    quicksort(a, n);
-   printf("\nAfter sorting: ");
+   cout << "\n" << "After sorting: ";
    for(i=0; i<n; i++){
-      printf("%d ",a[i]);
+      cout << a[i] << " " ;
    }
-   printf("\n");
+   cout << "\n";
    return 0;
 }
 
@@ -32,7 +33,6 @@ int push(int stack[MAX], int x, int (*top)){
         (*top) = (*top) + 1;
         stack[*top] = x;
     }
-    //printf("Inside push top = %d\n",stack[*top]);
     return 0;
 }
 
@@ -43,7 +43,6 @@ int pop(int stack[MAX], int (*x), int (*top)){
         (*x) = stack[*top];
         (*top) = (*top) - 1;
     }
-    //printf("Inside pop top = %d\n",(*x));
     return 0;
 }
 
@@ -70,43 +69,26 @@ int partition(int a[50], int (*pi), int low, int high){
    a[low] = a[up];
    a[up] = pivot;
    (*pi) = up;
-
-   /*printf("\nDisplay array: ");
-   for(int i=0; i < ((high-low)+1); i++){
-      printf("%d ",a[i]);
-   }
-   printf("\n");*/
    return 0;
 }
 
 int quicksort(int a[50], int n){
     int top , stack[MAX], l, b, pi;
     top = (-1);
-    //printf("before first 2 push\n");
     push(stack, 0, &top);
-    //printf("After first push\n");
-    //printf("top = %d\n",top);
     push(stack, (n-1), &top);
-    //printf("After second push\n");
-    //printf("top = %d\n",top);
 
     while(top >= 0){
         pop(stack, &b, &top);
-        //printf("After pop\n");
-        //printf("top = %d and l= %d\n",top,l);
         pop(stack, &l, &top);
-        //printf("After pop\n");
-        //printf("top = %d and b= %d\n",top,b);
 
         partition(a, &pi, l, b);
-        //printf("partition position = %d\n", pi);
+
         if((pi-1) > l){
-            //printf("before push low = %d, high = %d\n", l, pi-1);
             push(stack, l, &top);
             push(stack, (pi-1), &top);
         }
         if((pi+1) < b){
-            //printf("before push low = %d, high = %d\n", pi+1, b);
             push(stack, (pi+1), &top);
             push(stack, b, &top);
         }
