006ae06f54666663a9d5af1fcbe735bb6059e02f
Happy <poddarsayan22@gmail.com>
2021-12-22 23:39:27 +0530

    Add insert and display function

--- divide-and-conquer/stressens-matrix-multiplication.cpp
+++ divide-and-conquer/stressens-matrix-multiplication.cpp
@@ -4,22 +4,44 @@ using namespace std;
 
 int main()
 {
+    void insert(double [4][4]);
+    void display(double [4][4]);
     int n;
-    cout << "Enter the size of the array: ";
-    cin >> n;
+    // cout << "Enter the size of the array: ";
+    //cin >> n;
+    double a[4][4], b[4][4];
 
-    int matrix[n][n];
+    cout << "\n Insert the values in the matrix a: \n";
+    insert(a);
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> matrix[i][j];
+    cout << "\n Insert the values in the matrix b: \n";
+    insert(b);
 
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++) {
-            cout << matrix[i][j] << "\t";
+    cout << "\n a: \n";
+    display(a);
+
+    cout << "\n b: \n";
+    display(b);
+
+    return 0;
+}
+
+void insert(double x[4][4]){
+    double n;
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> n;
+            x[i][j] = n;
+        }
+   }
+}
+
+void display(double x[4][4]){
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++) {
+            cout << x[i][j] << "\t";
         }
         cout << "\n";
     }
-
-    return 0;
 }
