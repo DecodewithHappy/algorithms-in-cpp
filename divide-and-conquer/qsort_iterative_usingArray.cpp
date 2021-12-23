558bb1edc9ae512fd88600fd0882b26ead2341ab
Happy <poddarsayan22@gmail.com>
2021-12-23 09:35:47 +0530

    Change some variable names and add some comments

--- divide-and-conquer/qsort_iterative_usingArray.cpp
+++ divide-and-conquer/qsort_iterative_usingArray.cpp
@@ -8,7 +8,7 @@ int main(){
    int pop(int [], int *, int *);
    int partition(int [], int *, int, int);
    int quicksort(int [], int);
-   int a[50], n, i, x;
+   int a[50], n, i, item;  // a is the unordered array containing user given values, n is the number of elements, i is loop variable, item is the element to be pushed or popped from the stack
    cout << "Enter the total number of elements:" << "\n";
    cin >> n;
    cout << "Enter array elements:" << "\n";
@@ -25,28 +25,28 @@ int main(){
    return 0;
 }
 
-int push(int stack[MAX], int x, int (*top)){
+int push(int stack[MAX], int item, int (*top)){ 
     
     if((*top) == MAX-1){
         return 0;
     }else{
         (*top) = (*top) + 1;
-        stack[*top] = x;
+        stack[*top] = item;
     }
     return 0;
 }
 
-int pop(int stack[MAX], int (*x), int (*top)){
+int pop(int stack[MAX], int (*item), int (*top)){  
     if((*top) == (-1)){
         return 0;
     }else{
-        (*x) = stack[*top];
+        (*item) = stack[*top];
         (*top) = (*top) - 1;
     }
     return 0;
 }
 
-int partition(int a[50], int (*pi), int low, int high){
+int partition(int a[50], int (*pi), int low, int high){ // pi holds the index of pivot.  
    int temp;
    int pivot = a[low];
    int down = low;
@@ -73,24 +73,24 @@ int partition(int a[50], int (*pi), int low, int high){
 }
 
 int quicksort(int a[50], int n){
-    int top , stack[MAX], l, b, pi;
+    int top , stack[MAX], lbound, ubound, pi;
     top = (-1);
     push(stack, 0, &top);
     push(stack, (n-1), &top);
 
     while(top >= 0){
-        pop(stack, &b, &top);
-        pop(stack, &l, &top);
+        pop(stack, &ubound, &top);
+        pop(stack, &lbound, &top);
 
-        partition(a, &pi, l, b);
+        partition(a, &pi, lbound, ubound);
 
-        if((pi-1) > l){
-            push(stack, l, &top);
+        if((pi-1) > lbound){
+            push(stack, lbound, &top);
             push(stack, (pi-1), &top);
         }
-        if((pi+1) < b){
+        if((pi+1) < ubound){
             push(stack, (pi+1), &top);
-            push(stack, b, &top);
+            push(stack, ubound, &top);
         }
     }
 }
